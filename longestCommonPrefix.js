2ef9bbb9725b00b3945d7e975d04671aa8ca29b8
Cadell Singh <cadellsingh9@gmail.com>
2021-03-10 12:25:42 -0400

    exercise finished

---
+++ longestCommonPrefix.js
@@ -0,0 +1,30 @@
+const longestCommonPrefix = (strs) => {
+  const strsLen = strs.map((str) => str.length);
+  const shortestLen = Math.min(...strsLen);
+
+  let longestPrefix = [];
+  for (let i = 0; i < shortestLen; i++) {
+    const char = strs[0][i];
+
+    for (let j = 1; j < strs.length; j++) {
+      if (char === strs[j][i]) {
+        longestPrefix.push(char);
+      }
+    }
+  }
+
+  return getDuplicates(longestPrefix);
+};
+
+const getDuplicates = (arr) => {
+  let results = "";
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i + 1] === arr[i]) results += arr[i];
+  }
+
+  return results;
+};
+
+let strs = ["flower", "flow", "flight"];
+// strs = ["dog", "racecar", "car"];
+console.log(longestCommonPrefix(strs));
